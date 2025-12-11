38e70ce6c087b021e3272add1a9b62d488803ded
shaikshabreen <shaikshabreen14@gmail.com>
2025-12-11 12:01:07 +0530

    problems

---
+++ Day 2/Divisors.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Divisors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        System.out.println("Divisors of " + n + ":");
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
