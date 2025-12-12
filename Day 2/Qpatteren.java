13a110af4beb4a9e932a071d25ede1199992d7b4
shaikshabreen <shaikshabreen14@gmail.com>
2025-12-12 09:07:08 +0530

    programs

---
+++ Day 2/Qpatteren.java
@@ -0,0 +1,21 @@
+public class Qpatteren {
+    public static void main(String[] args) {
+        for(int i=0;i<6;i++){
+            for(int j =0;j<6;j++){
+                if(j==0&&i !=5||j==4&&i!=5||i==0&&j!=5||i==4&&j!=5||i>2&&i==j)
+                {
+                    System.out.print("*");}
+                    else
+                        {
+                            System.out.print(" ");
+
+                }
+
+            }
+            System.out.println();
+        }
+    }
+    
+    }
+
+
