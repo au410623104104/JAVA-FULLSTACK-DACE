07bed2af941a63b0c04c64c22a789bc9cecac028
au410623104104 <shabreenshaik14@gmail.com>
2025-12-17 21:51:06 +0530

    Add files via upload

---
+++ Day 6/CountVowels.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+public class CountVowels {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        sc.nextLine();
+        String s=sc.nextLine();
+        System.out.println(a+" "+s);
+
+    }
+    
+}
